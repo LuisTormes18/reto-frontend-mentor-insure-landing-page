469bc16f8554ef454f83cb531f23197f20a2624d
Luis Tormes <luisangeltormes@gmail.coom>
2021-06-02 15:37:00 -0400

    reparando errores

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ const $menu_movil = document.getElementById("menu-header");
 const $img_menu = document.getElementById("img-nabvar");
 
 $btn_hamburger.innerHTML = `<img src="./images/icon-hamburger.svg" alt="" />`;
-$menu_movil.style.visibility = "hidden";
+
+// const mediaqueryList = window.matchMedia("(max-width: 680px)");
+// mediaqueryList.addListener(()=>{
+// if(mediaqueryList.matches){
+// $menu_movil.style.visibility = "hidden";
+
+// }else{
+// $menu_movil.style.visibility = "visible";
+
+// }
+// })
 
 $btn_hamburger.addEventListener("click", () => {
   console.log("click");
